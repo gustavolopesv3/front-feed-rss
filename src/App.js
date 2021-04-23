4261ae710403913cb09129546d1dd2c784215207
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2021-04-22 23:12:14 -0500

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import Parser from "rss-parser";
+import { useEffect, useState } from "react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let parser = new Parser();
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    rss();
+  }, []);
+
+  async function rss() {
+    await parser
+      .parseURL("https://blog-rss-notices.herokuapp.com/posts/feed")
+      .then((res) => {
+        setPost(res.items);
+      });
+  }
+  console.log(post);
+  return post.map((i) => {
+    console.log(i.title);
+    return (
+      <div className="container">
+        <div className="card">
+          <img src={i.guid} alt="" />
+          <div>
+            <h1>{i.title}</h1>
+            <p>
+              {i.content} <a href={i.link}>[Leia Mais]</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  });
 }
 
 export default App;
